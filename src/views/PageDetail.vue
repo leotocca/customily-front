14a5d26ee61204636fb62b15ee6b4669955ec312
leotocca <leotoccaceli@gmail.com>
2024-04-18 11:49:21 -0300

    Project cleanup

---
+++ src/views/PageDetail.vue
@@ -0,0 +1,5 @@
+<template>
+  <main class="w-full min-h-screen flex justify-center items-center">
+    <h1 class="prose prose-2xl">Page detail view test</h1>
+  </main>
+</template>
---
+++ src/views/PageList.vue
@@ -0,0 +1,5 @@
+<template>
+  <main class="w-full min-h-screen flex justify-center items-center">
+    <h1 class="prose prose-2xl">TEST</h1>
+  </main>
+</template>
