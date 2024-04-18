d9db0003e673006e4d02f95ffdfee8be08846d9a
leotocca <leotoccaceli@gmail.com>
2024-04-18 11:41:31 -0300

    Added tailwind and axios with it's instance creation

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,8 +6,8 @@ defineProps<{
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
+    <h1 class="prose prose-2xl text-red-200">{{ msg }}</h1>
+    <h3 class="">
       You’ve successfully created a project with
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
